db6673c722c0e4b2ba308054d6ecd31ca836794e
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2024-01-23 23:57:55 +0100

    updated sorting algorithms

---
+++ 106-bitonic_sort.c
@@ -0,0 +1,101 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swaps - change two values in ascending or descending order
+ * @arr: array
+ * @item1: item one
+ * @item2: item two
+ * @order: 1: ascending order, 0 descending order
+ */
+ 
+void swaps(int arr[], int item1, int item2, int order)
+{
+	int temp;
+
+	if (order == (arr[item1] > arr[item2]))
+	{
+		temp = arr[item1];
+		arr[item1] = arr[item2];
+		arr[item2] = temp;
+	}
+}
+
+/**
+ * merges - sort bitonic sequences recursively in both orders
+ * @arr: array
+ * @low: first element
+ * @nelemnt: elements number
+ * @order: 1: ascending order, 0 descending order
+ */
+ 
+void merges(int arr[], int low, int nelemnt, int order)
+{
+	int mid, i;
+
+	if (nelemnt > 1)
+	{
+		mid = nelemnt / 2;
+		for (i = low; i < low + mid; i++)
+			swaps(arr, i, i + mid, order);
+		merges(arr, low, mid, order);
+		merges(arr, low + mid, mid, order);
+	}
+}
+
+/**
+ * bitonicsorts - bitonic sort algorithm implementation
+ * @arr: array
+ * @low: first element
+ * @nelemnt: number of elements
+ * @order: 1: ascending order, 0 descending order
+ * @size: array lenght
+ */
+ 
+void bitonicsorts(int arr[], int low, int nelemnt, int order, int size)
+{
+	int mid;
+
+	if (nelemnt > 1)
+	{
+		if (order >= 1)
+		{
+			printf("Merging [%i/%i] (UP):\n", nelemnt, size);
+			print_arrays(&arr[low], nelemnt);
+		}
+		else
+		{
+			printf("Merging [%i/%i] (DOWN):\n", nelemnt, size);
+			print_arrays(&arr[low], nelemnt);
+		}
+		mid = nelemnt / 2;
+		bitonicsorts(arr, low, mid, 1, size);
+		bitonicsorts(arr, low + mid, mid, 0, size);
+		merge(arr, low, nelemnt, order);
+		if (order <= 0)
+		{
+			printf("Result [%i/%i] (DOWN):\n", nelemnt, size);
+			print_arrays(&arr[low], nelemnt);
+		}
+		if (order >= 1)
+		{
+			printf("Result [%i/%i] (UP):\n", nelemnt, size);
+			print_arrays(&arr[low], nelemnt);
+		}
+	}
+}
+
+/**
+ * bitonic_sorts - prepare the terrain to bitonic sort algorithm
+ * @array: array
+ * @size: array lenght
+ */
+ 
+void bitonic_sorts(int *array, size_t size)
+{
+	int order = 1;
+
+	if (!array || size < 2)
+		return;
+	bitonicsorts(array, 0, size, order, size);
+}
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,34 +1,63 @@
 #include "sort.h"
-/**
- * selection_sorts - A function that sorts an array of integers in ascending
- * order using the Selection sort algorithm
- * @size: size of the array
- * @array: list with numbers
- */
-void selection_sorts(int *array, size_t size)
-{
-	size_t i, index;
-	int tmp, swap, flag = 0;
+#include <stdio.h>
 
-	if (array == NULL)
-		return;
-	for (i = 0; i < size; i++)
-	{
-		tmp = i;
-		flag = 0;
-		for (index = i + 1; index < size; index++)
-		{
-			if (array[tmp] > array[index])
-			{
-				tmp = index;
-				flag += 1;
-			}
-		}
-		swap = array[i];
-		array[i] = array[tmp];
-		array[tmp] = swap;
-		if (flag != 0)
-			print_arrays(array, size);
-	}
+/**
+ *calloc - this is a calloc function
+ *@nmemb: number of elemets
+ *@size: bit size of each element
+ *Return: pointer to memory assignement
+ */
+
+void *calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return ('\0');
+	p = malloc(nmemb * size);
+	if (p == '\0')
+		return ('\0');
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = '\0';
+	return (p);
 }
 
+/**
+ * counting_sorts - this is a counting sort method implementation
+ * @array: array to sort
+ * @size: array size
+ */
+ 
+void counting_sorts(int *array, size_t size)
+{
+	int index, maximun = 0, *counter = '\0', *tmp = '\0';
+	size_t i;
+
+	if (array == '\0' || size < 2)
+		return;
+	/* find maximun number */
+	for (i = 0; i < size; i++)
+		if (array[i] > maximun)
+			maximun = array[i];
+	counter = calloc(maximun + 1, sizeof(int));
+	tmp = calloc(size + 1, sizeof(int));
+	/* count the array elements */
+	for (i = 0; i < size; i++)
+		counter[array[i]]++;
+	/* get the accumulative values */
+	for (index = 1; index <= maximun; index++)
+		counter[index] += counter[index - 1];
+	print_arrays(counter, maximun + 1);
+	/* get the new array sorted */
+	for (i = 0; i < size; ++i)
+	{
+		tmp[counter[array[i]] - 1] = array[i];
+		counter[array[i]]--;
+	}
+	/* replace old array to new array sorted */
+	for (i = 0; i < size; i++)
+		array[i] = tmp[i];
+	free(tmp);
+	free(counter);
+}
