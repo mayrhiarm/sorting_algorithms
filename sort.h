13fb285ddc096b70a00a1e82fa09b9e5b59cfdaa
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2024-01-24 03:17:39 +0000

    updated cocktail_sorts_list

--- sort.h
+++ sort.h
@@ -24,7 +24,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void cocktail_sorts_list(listint_t **list);
+void cocktail_sort_list(listint_t **list);
 void counting_sorts(int *array, size_t size);
 void merge_sorts(int *array, size_t size);
 void heap_sorts(int *array, size_t size);
