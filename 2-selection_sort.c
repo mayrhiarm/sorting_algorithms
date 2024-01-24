9413446c33bfde19ecf1f88ce964d569cec398a0
Hammed Bankole <40712924+bankole874@users.noreply.github.com>
2024-01-24 03:06:04 +0000

    updated selection_sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,64 +1,33 @@
 #include "sort.h"
-#include <stdio.h>
-
 /**
- *_calloc - this is a _calloc function
- *@nmemb: number of elemets
- *@size: bit size of each element
- *Return: pointer to memory assignement
+ * selection_sort - function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @size: size of the array
+ * @array: list with numbers
  */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
+void selection_sort(int *array, size_t size)
 {
-	unsigned int i = 0;
-	char *p;
+	size_t i, index;
+	int tmp, swap, flag = 0;
 
-	if (nmemb == 0 || size == 0)
-		return ('\0');
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return ('\0');
-	for (i = 0; i < (nmemb * size); i++)
-		p[i] = '\0';
-	return (p);
-}
-
-/**
- * counting_sorts - this is a counting sort method implementation
- * @array: array to sort
- * @size: array size
- */
-
-void counting_sorts(int *array, size_t size)
-{
-	int index, maximun = 0, *counter = '\0', *tmp = '\0';
-	size_t i;
-
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
-	/* find maximun number */
 	for (i = 0; i < size; i++)
-		if (array[i] > maximun)
-			maximun = array[i];
-	counter = _calloc(maximun + 1, sizeof(int));
-	tmp = _calloc(size + 1, sizeof(int));
-	/* count the array elements */
-	for (i = 0; i < size; i++)
-		counter[array[i]]++;
-	/* get the accumulative values */
-	for (index = 1; index <= maximun; index++)
-		counter[index] += counter[index - 1];
-	print_array(counter, maximun + 1);
-	/* get the new array sorted */
-	for (i = 0; i < size; ++i)
 	{
-		tmp[counter[array[i]] - 1] = array[i];
-		counter[array[i]]--;
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
+		{
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
+		}
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)
+			print_array(array, size);
 	}
-	/* replace old array to new array sorted */
-	for (i = 0; i < size; i++)
-		array[i] = tmp[i];
-	free(tmp);
-	free(counter);
 }
-
